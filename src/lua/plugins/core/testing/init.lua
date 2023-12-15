2ac377a0c764511739d3d49db1be5b26a303dc76
null0xeth <112922737+null0xeth@users.noreply.github.com>
2023-12-15 17:12:53 +0100

    .

--- src/lua/plugins/core/testing/init.lua
+++ src/lua/plugins/core/testing/init.lua
@@ -72,15 +72,15 @@ local spec = {
       }
     end,
     config = function(_, opts)
-      local neotest_ns = vim.api.nvim_create_namespace("neotest")
-      vim.diagnostic.config({
-        virtual_text = {
-          format = function(diagnostic)
-            local message = diagnostic.message:gsub("\n", " "):gsub("\t", " "):gsub("%s+", " "):gsub("^%s+", "")
-            return message
-          end,
-        },
-      }, neotest_ns)
+      -- local neotest_ns = vim.api.nvim_create_namespace("neotest")
+      -- vim.diagnostic.config({
+      --   virtual_text = {
+      --     format = function(diagnostic)
+      --       local message = diagnostic.message:gsub("\n", " "):gsub("\t", " "):gsub("%s+", " "):gsub("^%s+", "")
+      --       return message
+      --     end,
+      --   },
+      -- }, neotest_ns)
       require("neotest").setup(opts)
     end,
   },
