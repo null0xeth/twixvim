c28cc33c7270df0306fb3ee933689c9122749bfc
null0xeth <112922737+null0xeth@users.noreply.github.com>
2023-12-15 22:31:00 +0100

    .

--- src/lua/framework/model/modules/lspmodule.lua
+++ src/lua/framework/model/modules/lspmodule.lua
@@ -60,46 +60,69 @@ local function init_lsp_config() --= memoize(function()
   local cachecontroller = get_obj("framework.controller.cachecontroller", "cachecontroller")
   local iconCache = cachecontroller:query("icons")
 
-  local lsp = {
-    -- float = {
-    --   focusable = true,
-    --   style = "minimal",
-    --   border = "rounded",
-    -- },
-    diagnostic = {
-      -- virtual_text = true,
-      virtual_text = { spacing = 4, prefix = "●" },
-      underline = true,
-      update_in_insert = false,
-      severity_sort = true,
-      float = {
-        focusable = true,
-        style = "minimal",
-        border = "rounded",
-      },
-      signs = {
-        text = {
-          [vim.diagnostic.severity.ERROR] = "",
-          [vim.diagnostic.severity.WARN] = "",
-          [vim.diagnostic.severity.INFO] = "",
-          [vim.diagnostic.severity.HINT] = "",
-        },
+  ---General diagnostics settings
+  vim.diagnostic.config({
+    severity_sort = true,
+    underline = true,
+    update_in_insert = false,
+    signs = true,
+    float = {
+      scope = "line",
+      border = "single",
+      source = "always",
+      focusable = false,
+      close_events = {
+        "BufLeave",
+        "CursorMoved",
+        "InsertEnter",
+        "FocusLost",
       },
     },
-  }
+    virtual_text = {
+      source = "always",
+    },
+  })
+
+  --local lsp = {
+  -- float = {
+  --   focusable = true,
+  --   style = "minimal",
+  --   border = "rounded",
+  -- },
+  -- diagnostic = {
+  --   virtual_text = { spacing = 4, prefix = "●" },
+  --   underline = true,
+  --   update_in_insert = false,
+  --   severity_sort = true,
+  --   float = {
+  --     focusable = true,
+  --     style = "minimal",
+  --     border = "rounded",
+  --   },
+  --   signs = {
+  --     text = {
+  --       [vim.diagnostic.severity.ERROR] = "",
+  --       [vim.diagnostic.severity.WARN] = "",
+  --       [vim.diagnostic.severity.INFO] = "",
+  --       [vim.diagnostic.severity.HINT] = "",
+  --     },
+  --   },
+  -- },
+  --}
+  --vim.diagnostic.config(lsp.diagnostic)
+
   local signs = {
-    { name = "DiagnosticSignError", text = "" },
-    { name = "DiagnosticSignWarn", text = "" },
-    { name = "DiagnosticSignHint", text = "" },
-    { name = "DiagnosticSignInfo", text = "" },
+    { Error = "" },
+    { Warn = "" },
+    { Hint = "" },
+    { Info = "" },
   }
 
-  for _, sign in ipairs(signs) do
-    vim.fn.sign_define(sign.name, { texthl = sign.name, text = sign.text, numhl = sign.name })
+  for type, icon in pairs(signs) do
+    local hl = "DiagnosticSign" .. type
+    vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
   end
-
   -- Diagnostic configuration
-  vim.diagnostic.config(lsp.diagnostic)
 
   -- Hover configuration
   --vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, lsp.float)
