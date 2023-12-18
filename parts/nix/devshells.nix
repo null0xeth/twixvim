879b4b3826c525ab155840f59499035a44d2422e
null0xeth <112922737+null0xeth@users.noreply.github.com>
2023-12-18 23:09:32 +0100

    .

--- parts/nix/devshells.nix
+++ parts/nix/devshells.nix
@@ -7,22 +7,41 @@
       config.allowUnfree = true;
     };
 
-    packages.default = inputs'.neovim-flake.packages.default;
+    packages.twixvim = inputs'.neovim-flake.packages.default;
 
-    devshells.default = {
-      devshell = { name = "Neovim Shell"; };
+    devshells = {
+      default = {
+        devshell = { name = "Neovim Shell"; };
 
-      packages = lib.attrValues {
-        #inherit (config.packages) default;
-        inherit (inputs'.nil.packages) nil;
-        inherit (pkgs.luajitPackages) jsregexp luacheck;
-        inherit (pkgs.nodePackages) jsonlint;
-        inherit (pkgs)
-        #clang-tools_16
-          nixfmt cmake lua-language-server marksman vscode-langservers-extracted
-          actionlint yaml-language-server alejandra nixpkgs-fmt statix vulnix
-          deadnix stylua prettierd yamlfix yamllint manix gcc gnumake helm-ls
-          typescript taplo vscode;
+        packages = lib.attrValues {
+          #inherit (config.packages) default;
+          inherit (inputs'.nil.packages) nil;
+          inherit (pkgs.luajitPackages) jsregexp luacheck;
+          inherit (pkgs.nodePackages) jsonlint;
+          inherit (pkgs)
+          #clang-tools_16
+            nixfmt cmake lua-language-server marksman
+            vscode-langservers-extracted actionlint yaml-language-server
+            alejandra nixpkgs-fmt statix vulnix deadnix stylua prettierd yamlfix
+            yamllint manix gcc gnumake helm-ls typescript taplo vscode;
+        };
+      };
+      portable = {
+        devshell = { name = "Neovim Shell"; };
+
+        packages = lib.attrValues {
+          #inherit (config.packages) default;
+          inherit (config.packages) twixvim;
+          inherit (inputs'.nil.packages) nil;
+          inherit (pkgs.luajitPackages) jsregexp luacheck;
+          inherit (pkgs.nodePackages) jsonlint;
+          inherit (pkgs)
+          #clang-tools_16
+            nixfmt cmake lua-language-server marksman
+            vscode-langservers-extracted actionlint yaml-language-server
+            alejandra nixpkgs-fmt statix vulnix deadnix stylua prettierd yamlfix
+            yamllint manix gcc gnumake helm-ls typescript taplo vscode;
+        };
       };
     };
   };
