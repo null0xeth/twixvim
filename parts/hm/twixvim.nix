5bef2c973e10dbe76a89668806c58c79e2d81283
null0xeth <112922737+null0xeth@users.noreply.github.com>
2023-12-25 22:19:33 +0100

    .

--- parts/hm/twixvim.nix
+++ parts/hm/twixvim.nix
@@ -1,65 +1,64 @@
-#{localFlake}:
-#inputs: {
-{
-  config,
-  inputs',
-  pkgs,
-  lib,
-  ...
-}:
-with lib; let
-  cfg = config.homeManagerModules.twixvim;
-in {
-  options = {
-    homeManagerModules.twixvim = {
-      enable = mkOption {
-        type = types.bool;
-        default = true;
-        description = "Enable Twixvim IDE";
-      };
-      settings = {
-        basic = mkOption {
-          type = types.nullOr types.bool;
-          description = "only the bare-minimum directory management functions of HM";
-          default = null;
-        };
-        development = {
-          enable = mkOption {
-            type = types.bool;
-            default = false;
-            description = "Work with a local copy of the source";
+# #{localFlake}:
+# #inputs: {
+# {
+#   config,
+#   inputs',
+#   pkgs,
+#   lib,
+#   ...
+# }:
+{moduleWithSystem, ...}: {
+  flake.homeManagerModules.twixvim = moduleWithSystem (
+    perSystem @ {inputs'}: nixos @ {
+      config,
+      lib,
+      pkgs,
+      ...
+    }:
+      with lib; let
+        cfg = config.homeManagerModules.twixvim;
+      in {
+        options = {
+          hmModules.twixvim = {
+            enable = mkOption {
+              type = types.bool;
+              default = true;
+              description = "Enable Twixvim IDE";
+            };
+            settings = {
+              development = {
+                enable = mkOption {
+                  type = types.bool;
+                  default = false;
+                  description = "Work with a local copy of the source";
+                };
+              };
+            };
           };
         };
-      };
-    };
-  };
 
-  config = mkIf cfg.enable (mkMerge [
-    #(mkIf (!cfg.settings.basic) {
-    {
-      assertions = [
-        {
-          assertion = cfg.settings.basic == null;
-          message = "`settings.basic` is deprecated. Please remove it from your config";
-        }
-      ];
-      home = {
-        packages = [
-          #localFlake.inputs.neovim-flake.packages.x86_64-linux.neovim #does not work...
-          inputs'.neovim-flake.packages.default
-          pkgs.vscode-extensions.vadimcn.vscode-lldb
-          pkgs.vscode
-        ];
-      };
-    }
-    (mkIf (!cfg.settings.development.enable) {
-      home.file = {
-        ".config/nvim" = {
-          enable = true;
-          source = ../../src;
-          recursive = true;
-        };
-      };
-    })
-  ]);
+        config = mkIf cfg.enable (mkMerge [
+          #(mkIf (!cfg.settings.basic) {
+          {
+            home = {
+              packages = [
+                #localFlake.inputs.neovim-flake.packages.x86_64-linux.neovim #does not work...
+                inputs'.neovim-flake.packages.default
+                pkgs.vscode-extensions.vadimcn.vscode-lldb
+                pkgs.vscode
+              ];
+            };
+          }
+          (mkIf (!cfg.settings.development.enable) {
+            home.file = {
+              ".config/nvim" = {
+                enable = true;
+                source = ../../src;
+                recursive = true;
+              };
+            };
+          })
+        ]);
+      }
+  );
 }
