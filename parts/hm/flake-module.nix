b7e9a326b877d513fd193899034b1f35afb86ffb
null0xeth <112922737+null0xeth@users.noreply.github.com>
2023-12-25 14:38:22 +0100

    .

--- parts/hm/flake-module.nix
+++ parts/hm/flake-module.nix
@@ -1,13 +1,11 @@
 {
   flake-parts-lib,
   self,
-  withSystem,
   ...
 }: let
   inherit (flake-parts-lib) importApply;
 in {
   flake.homeManagerModules.default = importApply ./twixvim.nix {
     localFlake = self;
-    inherit withSystem;
   };
 }
--- parts/hm/twixvim.nix
+++ parts/hm/twixvim.nix
@@ -1,7 +1,4 @@
-{
-  localFlake,
-  withSystem,
-}:
+{localFlake}:
 #inputs: {
 {
   config,
