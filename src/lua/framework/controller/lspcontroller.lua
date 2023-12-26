d4b034b13fea4c34767773c42d39c609f4ad4787
null0xeth <112922737+null0xeth@users.noreply.github.com>
2023-12-26 01:04:26 +0100

    .

--- src/lua/framework/controller/lspcontroller.lua
+++ src/lua/framework/controller/lspcontroller.lua
@@ -136,7 +136,7 @@ end
 ---@param opts table
 ---@param customAttach? function
 function LspController:setup_lsp_servers(_, opts, customAttach)
-  local plugz = get_module("lazy.core.config", "lazy.core.config").spec.plugins["neoconf.nvim"]
+  local plugz = require("lazy.core.config").spec.plugins["neoconf.nvim"]
   require("neoconf").setup(require("lazy.core.plugin").values(plugz, "opts", false))
   self:custom_on_attach(customAttach)
   self:init_lsp_servers(opts)
