76d9535d62b58ee11f6ed70353bad7a0ebcee9ec
null0xeth <112922737+null0xeth@users.noreply.github.com>
2024-02-11 23:39:49 +0100

    ansible stuff added

--- src/lua/plugins/core/languages/ansible.lua
+++ src/lua/plugins/core/languages/ansible.lua
@@ -1,7 +1,40 @@
-return {
+polish = function()
+  local function yaml_ft(path, bufnr)
+    -- get content of buffer as string
+    local content = vim.filetype.getlines(bufnr)
+    if type(content) == "table" then
+      content = table.concat(content, "\n")
+    end
+
+    -- check if file is in roles, tasks, or handlers folder
+    local path_regex = vim.regex("(tasks\\|roles\\|inventory\\|handlers)/")
+    if path_regex and path_regex:match_str(path) then
+      return "ansible"
+    end
+    -- check for known ansible playbook text and if found, return yaml.ansible
+    local regex = vim.regex("hosts:\\|tasks:")
+    if regex and regex:match_str(content) then
+      return "ansible"
+    end
+
+    -- return yaml if nothing else
+    return "yaml"
+  end
+
+  vim.filetype.add({
+    extension = {
+      yml = yaml_ft,
+      yaml = yaml_ft,
+    },
+  })
+end
+
+local spec = {
   {
     "pearofducks/ansible-vim",
+    ft = "ansible",
     event = "KindaLazy",
+    opts = {},
   },
 
   {
@@ -20,6 +53,7 @@ return {
       servers = {
         ansiblels = {
           filetypes = {
+            "ansible",
             "yaml.ansible",
           },
           cmd = {
@@ -32,7 +66,6 @@ return {
                 enabled = true,
                 lint = {
                   enabled = true,
-                  path = "ansible-lint",
                 },
               },
             },
