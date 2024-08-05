b4e57a97252b7d204196c10e5c3480da3d5c0c5a
null0xeth <112922737+null0xeth@users.noreply.github.com>
2024-08-05 16:15:11 +0200

    .

--- src/lua/plugins/secondary/editor.lua
+++ src/lua/plugins/secondary/editor.lua
@@ -168,27 +168,27 @@ local spec = {
         --   ["<BS>"] = "⌫",
         --   ["<space>"] = "󱁐",
         --   ["<Tab>"] = "󰌒",
-          ["<Esc>"] = "⎋",
-        },
-        --key_labels = { ["<leader>"] = "SPC" },
-        --triggers = "auto",
-        win = {
-          -- border = "single", -- none, single, double, shadow
-          -- position = "bottom", -- bottom, top
-          -- margin = { 1, 0, 1, 0 }, -- extra window margin [top, right, bottom, left]
-          -- padding = { 1, 1, 1, 1 }, -- extra window padding [top, right, bottom, left]
-          -- winblend = 0,
-        },
-        popup_mappings = {
-          scroll_down = "<PageDown>",
-          scroll_up = "<PageUp>",
-        },
-        layout = {
-          -- height = { min = 4, max = 25 }, -- min and max height of the columns
-          -- width = { min = 20, max = 50 }, -- min and max width of the columns
-          -- spacing = 1, -- spacing between columns
---[[           align = "left", -- align columns left, center or right ]]
-        },
+        ["<Esc>"] = "⎋",
+      },
+      --key_labels = { ["<leader>"] = "SPC" },
+      --triggers = "auto",
+      win = {
+        -- border = "single", -- none, single, double, shadow
+        -- position = "bottom", -- bottom, top
+        -- margin = { 1, 0, 1, 0 }, -- extra window margin [top, right, bottom, left]
+        -- padding = { 1, 1, 1, 1 }, -- extra window padding [top, right, bottom, left]
+        -- winblend = 0,
+      },
+      popup_mappings = {
+        scroll_down = "<PageDown>",
+        scroll_up = "<PageUp>",
+      },
+      layout = {
+        -- height = { min = 4, max = 25 }, -- min and max height of the columns
+        -- width = { min = 20, max = 50 }, -- min and max width of the columns
+        -- spacing = 1, -- spacing between columns
+        --[[           align = "left", -- align columns left, center or right ]]
+      },
 
       defaults = config.keymap_categories,
     },
@@ -197,6 +197,23 @@ local spec = {
       wk.setup(opts.setup)
       wk.register(opts.defaults)
     end,
+  },
+  {
+    "allaman/kustomize.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    ft = "yaml",
+    opts = {
+      enable_key_mappings = true,
+      enable_lua_snip = true,
+      validate = { kubeconform_args = { "--strict", "--ignore-missing-schemas" } },
+      build = {
+        additional_args = { "--enable-helm", "--load-restrictor=LoadRestrictionsNone" },
+      },
+      deprecations = { kube_version = "1.25" },
+      kinds = { show_filepath = true, show_line = true, exclude_pattern = "" },
+    },
   },
 }
 
