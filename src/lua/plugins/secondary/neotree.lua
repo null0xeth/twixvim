7f7e7b13f8d45825777af8f8362c86fa78731cf7
null0xeth <112922737+null0xeth@users.noreply.github.com>
2024-08-25 18:22:04 +0200

    .

--- src/lua/plugins/secondary/neotree.lua
+++ src/lua/plugins/secondary/neotree.lua
@@ -237,37 +237,37 @@ local spec = {
           --width = require("wuelnerdotexe.plugin.util").get_sidebar_width(),
           insert_as = "sibling",
           mappings = {
-            -- Transfer.nvim mapppings
-            uu = {
-              function(state)
-                vim.cmd("TransferUpload " .. state.tree:get_node().path)
-              end,
-              desc = "upload file or directory",
-              nowait = true,
-            },
-            -- download (sync files)
-            ud = {
-              function(state)
-                vim.cmd("TransferDownload" .. state.tree:get_node().path)
-              end,
-              desc = "download file or directory",
-              nowait = true,
-            },
-            -- diff directory with remote
-            uf = {
-              function(state)
-                local node = state.tree:get_node()
-                local context_dir = node.path
-                if node.type ~= "directory" then
-                  -- if not a directory
-                  -- one level up
-                  context_dir = context_dir:gsub("/[^/]*$", "")
-                end
-                vim.cmd("TransferDirDiff " .. context_dir)
-                vim.cmd("Neotree close")
-              end,
-              desc = "diff with remote",
-            },
+            -- -- Transfer.nvim mapppings
+            -- uu = {
+            --   function(state)
+            --     vim.cmd("TransferUpload " .. state.tree:get_node().path)
+            --   end,
+            --   desc = "upload file or directory",
+            --   nowait = true,
+            -- },
+            -- -- download (sync files)
+            -- ud = {
+            --   function(state)
+            --     vim.cmd("TransferDownload" .. state.tree:get_node().path)
+            --   end,
+            --   desc = "download file or directory",
+            --   nowait = true,
+            -- },
+            -- -- diff directory with remote
+            -- uf = {
+            --   function(state)
+            --     local node = state.tree:get_node()
+            --     local context_dir = node.path
+            --     if node.type ~= "directory" then
+            --       -- if not a directory
+            --       -- one level up
+            --       context_dir = context_dir:gsub("/[^/]*$", "")
+            --     end
+            --     vim.cmd("TransferDirDiff " .. context_dir)
+            --     vim.cmd("Neotree close")
+            --   end,
+            --   desc = "diff with remote",
+            -- },
             -- End of Transfer.nvim mappings
             ["<CR>"] = "open",
             ["<2-LeftMouse>"] = "open",
