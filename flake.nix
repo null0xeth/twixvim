9b4d6fa60b0ea8cf0a4e4b11fea98938ada77ba1
null0xeth <112922737+null0xeth@users.noreply.github.com>
2024-09-21 07:36:41 +0200

    .

--- flake.nix
+++ flake.nix
@@ -1,10 +1,10 @@
 {
-  # nixConfig = {
-  #   extra-substituters = ["https://nix-community.cachix.org"];
-  #   extra-trusted-public-keys = [
-  #     nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=
-  #   ];
-  # };
+  nixConfig = {
+    extra-substituters = ["https://nix-community.cachix.org"];
+    extra-trusted-public-keys = [
+      nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=
+    ];
+  };
   description = "Description for the project brrrt";
 
   inputs = {
@@ -18,13 +18,7 @@
 
     nil.url = "github:oxalica/nil";
     devshell.url = "github:numtide/devshell";
-    # nix2container.url = "github:nlewo/nix2container";
-    # nix2container.inputs.nixpkgs.follows = "nixpkgs";
-    # mk-shell-bin.url = "github:rrbutani/nix-mk-shell-bin";
     neovim-flake.url = "github:nix-community/neovim-nightly-overlay";
-    /*
-    pre-commit-hooks-nix.url = "github:cachix/pre-commit-hooks.nix";
-    */
     alejandra.url = "github:kamadorueda/alejandra/3.0.0";
   };
 
