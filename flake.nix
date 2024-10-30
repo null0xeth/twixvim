77eae91c930f979864d9359c78bd4ad343d5927f
null0xeth <112922737+null0xeth@users.noreply.github.com>
2024-10-30 12:42:49 +0100

    .

--- flake.nix
+++ flake.nix
@@ -13,24 +13,24 @@
       url = "github:hercules-ci/flake-parts";
       #inputs.nixpkgs.follows = "nixpkgs";
     };
-    rust-overlay = {
-      url = "github:oxalica/rust-overlay";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    # rust-overlay = {
+    #   url = "github:oxalica/rust-overlay";
+    #   inputs.nixpkgs.follows = "nixpkgs";
+    # };
     systems = {
       url = "github:nix-systems/default";
       #inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    nil = {
-      url = "github:oxalica/nil";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    devshell = {
-      url = "github:numtide/devshell";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    # nil = {
+    #   url = "github:oxalica/nil";
+    #   inputs.nixpkgs.follows = "nixpkgs";
+    # };
+    #
+    # devshell = {
+    #   url = "github:numtide/devshell";
+    #   inputs.nixpkgs.follows = "nixpkgs";
+    # };
 
     neovim-flake = {
       url = "github:nix-community/neovim-nightly-overlay";
@@ -54,7 +54,7 @@
       imports = [
         #./parts/flake-module.nix
         #inputs.agenix-rekey.flakeModule
-        inputs.devshell.flakeModule
+        #inputs.devshell.flakeModule
         ./parts/hm/flake-module.nix
       ];
 
@@ -71,77 +71,77 @@
           inherit system;
           config.allowUnfree = true;
           config.hostPlatform = system;
-          overlays = [
-            inputs.rust-overlay.overlays.default
-          ];
+          # overlays = [
+          #   inputs.rust-overlay.overlays.default
+          # ];
         };
 
-        devshells.default = {
-          devshell = {
-            name = "Neovim Devshell";
-
-            packages = with pkgs;
-              [
-                luajit_openresty
-                luajitPackages.jsregexp
-                luajitPackages.luacheck
-                python312Packages.pip
-
-                zulu17
-                julia_19-bin
-                hadolint
-                checkmake
-                gitlint
-                pgformatter
-                tfsec
-                hclfmt
-                yamlfix
-                #php84Packages.composer
-                nodePackages.jsonlint
-                rust-bin.stable.latest.default
-                ripgrep
-                nixfmt
-                cmake
-                ansible-language-server
-                ansible-lint
-                lua-language-server
-                marksman
-                vscode-langservers-extracted
-                actionlint
-                yaml-language-server
-                alejandra
-                nixpkgs-fmt
-                statix
-                vulnix
-                deadnix
-                stylua
-                prettierd
-                terraform-ls
-                tflint
-                shfmt
-                shellcheck
-                bash-language-server
-
-                yamllint
-                manix
-                gcc
-                gnumake
-                helm-ls
-                typescript
-                taplo
-                vscode
-                docker-compose-language-service
-                dockerfile-language-server-nodejs
-                gitlab-ci-ls
-                ruby-lsp
-                rubyPackages.solargraph
-                rubocop
-                rubyPackages.standard
-                rubyPackages.erb-formatter
-              ]
-              ++ [inputs'.nil.packages.default];
-          };
-        };
+        # devshells.default = {
+        #   devshell = {
+        #     name = "Neovim Devshell";
+        #
+        #     packages = with pkgs;
+        #       [
+        #         luajit_openresty
+        #         luajitPackages.jsregexp
+        #         luajitPackages.luacheck
+        #         python312Packages.pip
+        #
+        #         zulu17
+        #         julia_19-bin
+        #         hadolint
+        #         checkmake
+        #         gitlint
+        #         pgformatter
+        #         tfsec
+        #         hclfmt
+        #         yamlfix
+        #         #php84Packages.composer
+        #         nodePackages.jsonlint
+        #         rust-bin.stable.latest.default
+        #         ripgrep
+        #         nixfmt
+        #         cmake
+        #         ansible-language-server
+        #         ansible-lint
+        #         lua-language-server
+        #         marksman
+        #         vscode-langservers-extracted
+        #         actionlint
+        #         yaml-language-server
+        #         alejandra
+        #         nixpkgs-fmt
+        #         statix
+        #         vulnix
+        #         deadnix
+        #         stylua
+        #         prettierd
+        #         terraform-ls
+        #         tflint
+        #         shfmt
+        #         shellcheck
+        #         bash-language-server
+        #
+        #         yamllint
+        #         manix
+        #         gcc
+        #         gnumake
+        #         helm-ls
+        #         typescript
+        #         taplo
+        #         vscode
+        #         docker-compose-language-service
+        #         dockerfile-language-server-nodejs
+        #         gitlab-ci-ls
+        #         ruby-lsp
+        #         rubyPackages.solargraph
+        #         rubocop
+        #         rubyPackages.standard
+        #         rubyPackages.erb-formatter
+        #       ]
+        #       ++ [inputs'.nil.packages.default];
+        #   };
+        #};
       };
     };
 }
