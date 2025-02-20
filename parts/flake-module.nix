98936b748c9abece72f2c188f49e3fb5fc10bf39
null0xeth <112922737+null0xeth@users.noreply.github.com>
2025-02-20 05:35:45 +0100

    feat: Refactored gitlab ci file

--- parts/flake-module.nix
+++ parts/flake-module.nix
@@ -1,90 +1,90 @@
 {inputs, ...}: {
   imports = [
-    inputs.devshell.flakeModule
+    #inputs.devshell.flakeModule
     ./hm/flake-module.nix
   ];
 
- # perSystem = {
+  # perSystem = {
   #  config,
- #   pkgs,
-#    system,
-#    inputs',
-#    ...
-#  }: {
-#    _module.args.pkgs = import inputs.nixpkgs {
-#      inherit system;
-#      config.allowUnfree = true;
-#      config.hostPlatform = system;
-#      overlays = [
-#        inputs.rust-overlay.overlays.default
-#      ];
-#    };
+  #   pkgs,
+  #    system,
+  #    inputs',
+  #    ...
+  #  }: {
+  #    _module.args.pkgs = import inputs.nixpkgs {
+  #      inherit system;
+  #      config.allowUnfree = true;
+  #      config.hostPlatform = system;
+  #      overlays = [
+  #        inputs.rust-overlay.overlays.default
+  #      ];
+  #    };
 
-    # devshells.default = {
-    #   devshell = {
-    #     name = "Neovim Devshell";
-    #   };
-    #   env = [];
-    #   packages = with pkgs;
-    #     [
-    #       luajit_openresty
-    #       luajitPackages.jsregexp
-    #       luajitPackages.luacheck
-    #       python312Packages.pip
-    #
-    #       zulu17
-    #       julia_19-bin
-    #       hadolint
-    #       checkmake
-    #       gitlint
-    #       pgformatter
-    #       tfsec
-    #       hclfmt
-    #       yamlfix
-    #       #php84Packages.composer
-    #       nodePackages.jsonlint
-    #       rust-bin.stable.latest.default
-    #       ripgrep
-    #       nixfmt
-    #       cmake
-    #       ansible-language-server
-    #       ansible-lint
-    #       lua-language-server
-    #       marksman
-    #       vscode-langservers-extracted
-    #       actionlint
-    #       yaml-language-server
-    #       alejandra
-    #       nixpkgs-fmt
-    #       statix
-    #       vulnix
-    #       deadnix
-    #       stylua
-    #       prettierd
-    #       terraform-ls
-    #       tflint
-    #       shfmt
-    #       shellcheck
-    #       bash-language-server
-    #
-    #       yamllint
-    #       manix
-    #       gcc
-    #       gnumake
-    #       helm-ls
-    #       typescript
-    #       taplo
-    #       vscode
-    #       docker-compose-language-service
-    #       dockerfile-language-server-nodejs
-    #       gitlab-ci-ls
-    #       ruby-lsp
-    #       rubyPackages.solargraph
-    #       rubocop
-    #       rubyPackages.standard
-    #       rubyPackages.erb-formatter
-    #     ]
-    #     ++ [inputs'.nil.packages.default];
-    # };
- # };
+  # devshells.default = {
+  #   devshell = {
+  #     name = "Neovim Devshell";
+  #   };
+  #   env = [];
+  #   packages = with pkgs;
+  #     [
+  #       luajit_openresty
+  #       luajitPackages.jsregexp
+  #       luajitPackages.luacheck
+  #       python312Packages.pip
+  #
+  #       zulu17
+  #       julia_19-bin
+  #       hadolint
+  #       checkmake
+  #       gitlint
+  #       pgformatter
+  #       tfsec
+  #       hclfmt
+  #       yamlfix
+  #       #php84Packages.composer
+  #       nodePackages.jsonlint
+  #       rust-bin.stable.latest.default
+  #       ripgrep
+  #       nixfmt
+  #       cmake
+  #       ansible-language-server
+  #       ansible-lint
+  #       lua-language-server
+  #       marksman
+  #       vscode-langservers-extracted
+  #       actionlint
+  #       yaml-language-server
+  #       alejandra
+  #       nixpkgs-fmt
+  #       statix
+  #       vulnix
+  #       deadnix
+  #       stylua
+  #       prettierd
+  #       terraform-ls
+  #       tflint
+  #       shfmt
+  #       shellcheck
+  #       bash-language-server
+  #
+  #       yamllint
+  #       manix
+  #       gcc
+  #       gnumake
+  #       helm-ls
+  #       typescript
+  #       taplo
+  #       vscode
+  #       docker-compose-language-service
+  #       dockerfile-language-server-nodejs
+  #       gitlab-ci-ls
+  #       ruby-lsp
+  #       rubyPackages.solargraph
+  #       rubocop
+  #       rubyPackages.standard
+  #       rubyPackages.erb-formatter
+  #     ]
+  #     ++ [inputs'.nil.packages.default];
+  # };
+  # };
 }
